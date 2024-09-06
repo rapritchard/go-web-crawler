bfbfcf10e0bfd43885801b7bd91973ab102390c4
Reece Pritchard <rapritchard96@gmail.com>
2024-09-06 17:11:24 +0100

    feat: setup command line argument

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	// args without the path to the program
+	args := os.Args[1:]
+
+	if len(args) < 1 {
+		fmt.Println("no website provided")
+		os.Exit(1)
+	}
+
+	if len(args) > 1 {
+		fmt.Println("too many arguments provided")
+		os.Exit(1)
+	}
+
+	baseURL := args[0]
+	fmt.Printf("starting crawl of: %v\n", baseURL)
 }
